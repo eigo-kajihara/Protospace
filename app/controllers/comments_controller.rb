e60b9effdd99a581008b26ee9586e0598c413ba4
kawahara-yuri <as.22.kawahara@gmail.com>
2018-09-13 20:37:05 +0900

    Revert "コメント機能編集"
    This reverts commit 2a0cd9b511a62b4636afec1c5312b8f468bb5fac.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    @prototype = Prototype.find(params[:id])
+    @comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to prototype_path(params[:prototype_id]) }
+      format.json
+    end
   end
 
   private
