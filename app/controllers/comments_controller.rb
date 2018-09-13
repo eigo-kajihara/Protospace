2a0cd9b511a62b4636afec1c5312b8f468bb5fac
kawahara-yuri <as.22.kawahara@gmail.com>
2018-09-13 20:36:34 +0900

    コメント機能編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
-    respond_to do |format|
-      format.html { redirect_to prototype_path(params[:prototype_id]) }
-      format.json
-    end
+    Comment.create(comment_params)
+    @prototype = Prototype.find(params[:id])
   end
 
   private
