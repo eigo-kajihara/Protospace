5200df967639d50d57d2cd4c9a6157efee876d7d
kawahara-yuri <as.22.kawahara@gmail.com>
2018-09-21 17:35:01 +0900

    Merge branch 'master' into comment
    # Conflicts:
    #	config/routes.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Miuraspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.test_framework       false
       g.helper               false
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show] do
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
     resources :comments, only: [:create, :destroy, :edit, :update]
   end
   resources :users, only: [:show, :edit, :update]
