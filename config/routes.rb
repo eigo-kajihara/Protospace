ddba3951c04d798b28b28de309967e64ddd00074
eigo-kajihara <2bijmd3@gmail.com>
2018-09-21 17:08:27 +0900

    Newest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
+  get 'prototypes/newest' => 'prototypes#newest'
   resources :prototypes, only: [:index, :new, :create, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
