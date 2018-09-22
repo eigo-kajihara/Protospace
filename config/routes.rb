411442464c0cf2b911a9762f036b9c3e82dd9dd0
hitomihitsudai <murotoshittoro333@gmail.com>
2018-09-22 10:59:51 +0900

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-<<<<<<< HEAD
-
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
-    resources :comments, only: [:create, :destroy, :edit, :update]
-=======
   get 'prototypes/newest' => 'prototypes#newest'
   resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
    resources :comments, only: [:create, :destroy, :edit, :update]
->>>>>>> master
   end
   resources :users, only: [:show, :edit, :update]
   get   '/tags'  =>  'prototypes#tags'       #tag一覧画面
